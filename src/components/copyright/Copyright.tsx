58547658124fb8f684b80678bd336e2dd920ea32
coturiv <coturiv@gmail.com>
2026-02-13 00:10:21 +0900

    Add social links

--- src/components/copyright/Copyright.tsx
+++ src/components/copyright/Copyright.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Copyright.css';
+import { IonIcon } from '@ionic/react';
+import { logoGithub, logoX } from 'ionicons/icons';
 
 const Copyright: React.FC = () => {
   const startYear = 2020;
@@ -15,6 +17,16 @@ const Copyright: React.FC = () => {
     <div className="copyright-container">
       <div className="copyright-content">
         <span className="copyright-text">© {startYear} - {currentYear} coturiv. All rights reserved.</span>
+        <div className="social-links">
+          <a href="https://github.com/coturiv/crypto-i" target="_blank" rel="noopener noreferrer" aria-label="GitHub repository">
+            <IonIcon icon={logoGithub} className="social-icon" />
+          </a>
+          <a href="https://x.com/coturiv" target="_blank" rel="noopener noreferrer" aria-label="X profile">
+            <IonIcon icon={logoX} className="social-icon" />
+          </a>
+        </div>
+      </div>
+      <div className="updated-row">
         <span className="updated-text">Latest updated: {today}</span>
       </div>
     </div>
